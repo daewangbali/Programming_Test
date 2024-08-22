1d1e42d735eb78a5dff9d07e6381a2d1abd85d51
dawoon jung <daewangbali@gmail.com>
2024-08-22 15:00:49 +0900

    Problem solving

--- src/main/java/org/problems/problem04/Main.java
+++ src/main/java/org/problems/problem04/Main.java
@@ -1,20 +1,23 @@
 package org.problems.problem04;
 
+import java.math.BigInteger;
 public class Main {
     public static void main(String[] args) {
-        int result = factorial(4);
+        BigInteger result = factorial(1000);
         System.out.println(result);
     }
 
     //재귀함수를 반복문으로 대체
-    static int factorial(int n){
-        int result = 1;
+    static BigInteger factorial(int n){
+        BigInteger result = BigInteger.ONE;
 
         while(n > 0){
-            result *= n;
+            result = result.multiply(BigInteger.valueOf(n));
             n--;
         }
+
         return result;
+
     }
 
 }
